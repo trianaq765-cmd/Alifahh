ba5db09412b4fd57bbe17057b7ab825b666a34ee
trianaq765-cmd <trianaq765@gmail.com>
2026-01-17 19:22:13 +0600

    Update luaRunner.js

--- utils/luaRunner.js
+++ utils/luaRunner.js
@@ -1,7 +1,21 @@
 /**
- * Meson Obfuscator v6.0 - Stable & Professional
- * Based on working v3.1 + improved output formatting
+ * Meson Obfuscator v7.0 - Complete Professional Version
+ * All transforms + Anti-Tamper + Anti-Debug
  */
+
+// Import transforms
+const StringEncrypt = require('./transforms/stringEncrypt');
+const NumberEncode = require('./transforms/numberEncode');
+const ControlFlow = require('./transforms/controlFlow');
+const DeadCode = require('./transforms/deadCode');
+const OpaquePredicates = require('./transforms/opaquePredicates');
+const StringSplit = require('./transforms/stringSplit');
+const FunctionWrap = require('./transforms/functionWrap');
+const TableIndirect = require('./transforms/tableIndirect');
+const ProxyFunction = require('./transforms/proxyFunction');
+const JunkLoops = require('./transforms/junkLoops');
+const AntiTamper = require('./transforms/antiTamper');
+const AntiDebug = require('./transforms/antiDebug');
 
 class MesonObfuscator {
     constructor() {
@@ -15,7 +29,7 @@ class MesonObfuscator {
     }
 
     // ==================== NAME GENERATION ====================
-    
+
     generateVarName(style = 'mixed') {
         const styles = {
             single: () => {
@@ -50,38 +64,20 @@ class MesonObfuscator {
                 }
                 return '_' + (this.varCounter++);
             },
-            O0o: () => {
-                let name = '';
-                for (let i = 0; i < 6; i++) {
-                    name += ['O', 'o', '0'][Math.floor(Math.random() * 3)];
-                }
-                if (!this.usedNames.has(name)) {
-                    this.usedNames.add(name);
-                    return name;
-                }
-                return '_' + (this.varCounter++);
-            },
             mixed: () => {
-                const all = ['single', 'withNum', 'Il1', 'O0o'];
+                const all = ['single', 'withNum', 'Il1'];
                 return styles[all[Math.floor(Math.random() * all.length)]]();
             }
         };
-        
         return styles[style] ? styles[style]() : styles.mixed();
     }
 
-    // Format number with variety (hex, binary, decimal, underscore)
     formatNumber(num) {
         if (num < 0) return '(-' + this.formatNumber(-num) + ')';
-        
         const r = Math.random();
         if (r < 0.20) return '0x' + num.toString(16).toUpperCase();
         if (r < 0.35) return '0X' + num.toString(16);
         if (r < 0.45 && num < 256) return '0b' + num.toString(2);
-        if (r < 0.55 && num < 256) return '0B' + num.toString(2);
-        if (r < 0.65 && num > 1000) {
-            return num.toString().replace(/\B(?=(\d{2})+(?!\d))/g, '_');
-        }
         return num.toString();
     }
 
@@ -105,10 +101,6 @@ class MesonObfuscator {
                 case 'advanced':
                     output = this.applyAdvancedObfuscation(sourceCode);
                     break;
-                case 'vm':
-                case 'ultimate':
-                    output = this.applyVMObfuscation(sourceCode);
-                    break;
                 default:
                     output = this.applyBasicObfuscation(sourceCode);
             }
@@ -131,185 +123,116 @@ class MesonObfuscator {
         }
     }
 
-    // ==================== TIER IMPLEMENTATIONS ====================
+    // ==================== BASIC TIER ====================
 
     applyBasicObfuscation(code) {
         code = this.removeComments(code);
-        code = this.encryptStrings(code);
+        
+        // String Encryption
+        const stringEnc = new StringEncrypt(this);
+        code = stringEnc.apply(code);
+        
         code = this.minify(code);
-        return '--[[Meson v6.0]]\n' + code;
+        return '--[[Meson v7.0]]\n' + code;
     }
+
+    // ==================== STANDARD TIER ====================
 
     applyStandardObfuscation(code) {
         code = this.removeComments(code);
-        code = this.encryptStrings(code);
-        code = this.encodeNumbers(code);
+        
+        // String Encryption
+        const stringEnc = new StringEncrypt(this);
+        code = stringEnc.apply(code);
+        
+        // Number Encoding
+        const numEnc = new NumberEncode(this);
+        code = numEnc.apply(code);
+        
+        // Dead Code
+        const deadCode = new DeadCode(this);
+        code = deadCode.apply(code);
+        
+        // Variable Renaming
         code = this.renameVariables(code);
-        code = this.addDeadCode(code);
+        
         code = this.minify(code);
-        return '--[[Meson v6.0]]\n' + code;
+        return '--[[Meson v7.0]]\n' + code;
     }
+
+    // ==================== ADVANCED TIER ====================
 
     applyAdvancedObfuscation(code) {
         code = this.removeComments(code);
-        code = this.addDeadCode(code);
-        code = this.encryptStrings(code);
-        code = this.encodeNumbers(code);
-        code = this.addOpaquePredicates(code);
+        
+        // Anti-Tamper (First layer of protection)
+        const antiTamper = new AntiTamper(this);
+        code = antiTamper.apply(code);
+        
+        // Anti-Debug
+        const antiDebug = new AntiDebug(this);
+        code = antiDebug.apply(code);
+        
+        // Function Wrapping
+        const funcWrap = new FunctionWrap(this);
+        code = funcWrap.apply(code);
+        
+        // String Splitting (before encryption)
+        const stringSplit = new StringSplit(this);
+        code = stringSplit.apply(code);
+        
+        // String Encryption
+        const stringEnc = new StringEncrypt(this);
+        code = stringEnc.apply(code);
+        
+        // Number Encoding
+        const numEnc = new NumberEncode(this);
+        code = numEnc.apply(code);
+        
+        // Proxy Functions
+        const proxyFunc = new ProxyFunction(this);
+        code = proxyFunc.apply(code);
+        
+        // Table Indirection
+        const tableInd = new TableIndirect(this);
+        code = tableInd.apply(code);
+        
+        // Dead Code
+        const deadCode = new DeadCode(this);
+        code = deadCode.apply(code);
+        
+        // Junk Loops
+        const junkLoops = new JunkLoops(this);
+        code = junkLoops.apply(code);
+        
+        // Opaque Predicates
+        const opaque = new OpaquePredicates(this);
+        code = opaque.apply(code);
+        
+        // Control Flow
+        const controlFlow = new ControlFlow(this);
+        code = controlFlow.apply(code);
+        
+        // Variable Renaming (last step)
         code = this.renameVariables(code);
-        code = this.wrapControlFlow(code);
+        
         code = this.minify(code);
-        return '--[[Meson v6.0]]\n' + code;
+        return '--[[Meson v7.0|Protected]]\n' + code;
     }
 
-    applyVMObfuscation(sourceCode) {
-        this.reset();
-        const code = this.removeComments(sourceCode);
-        return this.buildVM(code);
+    // ==================== HELPER METHODS ====================
+
+    removeComments(code) {
+        code = code.replace(/--\[\[[\s\S]*?\]\]/g, '');
+        code = code.replace(/--[^\n]*/g, '');
+        return code;
     }
-
-    // ==================== VM BUILDER (TESTED & WORKING) ====================
-
-    buildVM(sourceCode) {
-        // Generate unique short variable names
-        const v = {
-            xor: this.generateVarName('single'),
-            key: this.generateVarName('single'),
-            data: this.generateVarName('single'),
-            decode: this.generateVarName('single'),
-            result: this.generateVarName('single'),
-            chunk: this.generateVarName('single'),
-            err: this.generateVarName('single'),
-            char: this.generateVarName('single'),
-            concat: this.generateVarName('single'),
-            i: this.generateVarName('single'),
-            a: this.generateVarName('single'),
-            b: this.generateVarName('single'),
-            p: this.generateVarName('single'),
-            c: this.generateVarName('single'),
-            m: this.generateVarName('single'),
-            n: this.generateVarName('single'),
-            r: this.generateVarName('single'),
-            t: this.generateVarName('single'),
-        };
-
-        // Encryption key
-        const key = Math.floor(Math.random() * 200) + 50;
-
-        // Encrypt source code
-        const encrypted = [];
-        for (let i = 0; i < sourceCode.length; i++) {
-            encrypted.push(sourceCode.charCodeAt(i) ^ key);
-        }
-
-        // Format encrypted data with mixed number formats
-        const formattedData = encrypted.map(b => this.formatNumber(b)).join(',');
-
-        // Build the VM code
-        const vmCode = `--[[Meson v6.0|VM Protected]]
-return(function(...)
-local ${v.char}=string.char
-local ${v.concat}=table.concat
-local ${v.xor}=bit32 and bit32.bxor or bit and bit.bxor or function(${v.a},${v.b})
-local ${v.p},${v.c}=1,0
-while ${v.a}>0 and ${v.b}>0 do
-local ${v.m},${v.n}=${v.a}%2,${v.b}%2
-if ${v.m}~=${v.n} then ${v.c}=${v.c}+${v.p} end
-${v.a},${v.b},${v.p}=(${v.a}-${v.m})/2,(${v.b}-${v.n})/2,${v.p}*2
-end
-if ${v.a}<${v.b} then ${v.a}=${v.b} end
-while ${v.a}>0 do
-local ${v.m}=${v.a}%2
-if ${v.m}>0 then ${v.c}=${v.c}+${v.p} end
-${v.a},${v.p}=(${v.a}-${v.m})/2,${v.p}*2
-end
-return ${v.c}
-end
-local ${v.key}=${this.formatNumber(key)}
-local ${v.data}={${formattedData}}
-local ${v.decode}=function()
-local ${v.r}={}
-for ${v.i}=1,#${v.data} do
-${v.r}[${v.i}]=${v.char}(${v.xor}(${v.data}[${v.i}],${v.key}))
-end
-return ${v.concat}(${v.r})
-end
-local ${v.result}=${v.decode}()
-local ${v.chunk},${v.err}=(loadstring or load)(${v.result})
-if not ${v.chunk} then
-error("Meson:"..tostring(${v.err}))
-end
-return ${v.chunk}(...)
-end)(...)`;
-
-        return this.minify(vmCode);
-    }
-
-    // ==================== STRING ENCRYPTION ====================
-
-    encryptStrings(code) {
-        const key = Math.floor(Math.random() * 200) + 50;
-        const funcName = this.generateVarName('withNum');
-        const keyVar = this.generateVarName('single');
-        const tblVar = this.generateVarName('single');
-        const resVar = this.generateVarName('single');
-        const iVar = this.generateVarName('single');
-        const aVar = this.generateVarName('single');
-        const bVar = this.generateVarName('single');
-        const pVar = this.generateVarName('single');
-        const cVar = this.generateVarName('single');
-        const mVar = this.generateVarName('single');
-        const nVar = this.generateVarName('single');
-
-        const decoder = `local ${funcName};do local ${keyVar}=${this.formatNumber(key)};${funcName}=function(${tblVar})local ${resVar}=""for ${iVar}=1,#${tblVar} do ${resVar}=${resVar}..string.char((bit32 and bit32.bxor or bit and bit.bxor or function(${aVar},${bVar})local ${pVar},${cVar}=1,0 while ${aVar}>0 and ${bVar}>0 do local ${mVar},${nVar}=${aVar}%2,${bVar}%2 if ${mVar}~=${nVar} then ${cVar}=${cVar}+${pVar} end ${aVar},${bVar},${pVar}=(${aVar}-${mVar})/2,(${bVar}-${nVar})/2,${pVar}*2 end if ${aVar}<${bVar} then ${aVar}=${bVar} end while ${aVar}>0 do local ${mVar}=${aVar}%2 if ${mVar}>0 then ${cVar}=${cVar}+${pVar} end ${aVar},${pVar}=(${aVar}-${mVar})/2,${pVar}*2 end return ${cVar} end)(${tblVar}[${iVar}],${keyVar}))end return ${resVar} end end;`;
-
-        const stringPattern = /(["'])(?:(?!\1)[^\\]|\\.)*\1/g;
-
-        const result = code.replace(stringPattern, (match) => {
-            const content = match.slice(1, -1);
-            if (content.length < 2 || content.includes('\\')) return match;
-
-            const encrypted = [];
-            for (let i = 0; i < content.length; i++) {
-                encrypted.push(content.charCodeAt(i) ^ key);
-            }
-            return funcName + '({' + encrypted.map(b => this.formatNumber(b)).join(',') + '})';
-        });
-
-        return decoder + result;
-    }
-
-    // ==================== NUMBER ENCODING ====================
-
-    encodeNumbers(code) {
-        const numberPattern = /(?<![.\w])(\d+)(?![.\dxXbB])/g;
-
-        return code.replace(numberPattern, (match, num) => {
-            const n = parseInt(num);
-            if (n < 2 || n > 99999 || Math.random() > 0.5) return match;
-
-            const methods = [
-                () => {
-                    const a = Math.floor(Math.random() * 500);
-                    return '(' + this.formatNumber(a) + '+' + this.formatNumber(n - a) + ')';
-                },
-                () => {
-                    const a = n + Math.floor(Math.random() * 500);
-                    return '(' + this.formatNumber(a) + '-' + this.formatNumber(a - n) + ')';
-                },
-                () => this.formatNumber(n)
-            ];
-
-            return methods[Math.floor(Math.random() * methods.length)]();
-        });
-    }
-
-    // ==================== VARIABLE RENAMING ====================
 
     renameVariables(code) {
         const protectedNames = this.getProtectedNames();
         const renames = new Map();
 
+        // Find local variable declarations
         const localPattern = /\blocal\s+([a-zA-Z_][a-zA-Z0-9_]*)/g;
         let match;
         while ((match = localPattern.exec(code)) !== null) {
@@ -319,6 +242,7 @@ end)(...)`;
             }
         }
 
+        // Find function parameters
         const funcPattern = /function\s*[^(]*\(([^)]*)\)/g;
         while ((match = funcPattern.exec(code)) !== null) {
             const params = match[1].split(',').map(p => p.trim()).filter(p => p && p !== '...');
@@ -329,6 +253,7 @@ end)(...)`;
             }
         }
 
+        // Apply renames
         let result = code;
         renames.forEach((newName, oldName) => {
             const regex = new RegExp('\\b' + oldName + '\\b', 'g');
@@ -338,84 +263,12 @@ end)(...)`;
         return result;
     }
 
-    // ==================== DEAD CODE ====================
-
-    addDeadCode(code) {
-        const snippets = [];
-        const count = Math.floor(Math.random() * 4) + 2;
-
-        for (let i = 0; i < count; i++) {
-            const v = this.generateVarName();
-            const type = Math.floor(Math.random() * 5);
-
-            switch (type) {
-                case 0:
-                    snippets.push('local ' + v + '=' + this.formatNumber(Math.floor(Math.random() * 10000)) + ';');
-                    break;
-                case 1:
-                    snippets.push('local ' + v + '=function()return nil end;');
-                    break;
-                case 2:
-                    const a = Math.floor(Math.random() * 100) + 100;
-                    snippets.push('if ' + this.formatNumber(a) + '>' + this.formatNumber(a + 50) + ' then local ' + v + '=nil end;');
-                    break;
-                case 3:
-                    snippets.push('local ' + v + '={};');
-                    break;
-                case 4:
-                    snippets.push('local ' + v + '=string.rep("",0);');
-                    break;
-            }
-        }
-
-        return snippets.join('') + code;
-    }
-
-    // ==================== OPAQUE PREDICATES ====================
-
-    addOpaquePredicates(code) {
-        const checkVar = this.generateVarName();
-        const predicates = [
-            '(' + this.formatNumber(Math.floor(Math.random() * 100) + 1) + '*' + this.formatNumber(Math.floor(Math.random() * 100) + 1) + '>=' + this.formatNumber(1) + ')',
-            '(type("")=="string")',
-            '(type({})=="table")',
-            '(#""==' + this.formatNumber(0) + ')'
-        ];
-
-        const pred = predicates[Math.floor(Math.random() * predicates.length)];
-        return 'local ' + checkVar + '=' + pred + ';if not ' + checkVar + ' then return end;' + code;
-    }
-
-    // ==================== CONTROL FLOW ====================
-
-    wrapControlFlow(code) {
-        const stateVar = this.generateVarName();
-        const funcVar = this.generateVarName();
-
-        return 'local ' + stateVar + '=' + this.formatNumber(1) + ';local ' + funcVar + '={[' + this.formatNumber(1) + ']=function()' + code + ';' + stateVar + '=' + this.formatNumber(0) + ' end};while ' + stateVar + '>' + this.formatNumber(0) + ' do if ' + funcVar + '[' + stateVar + '] then ' + funcVar + '[' + stateVar + ']() end end;';
-    }
-
-    // ==================== UTILITIES ====================
-
-    removeComments(code) {
-        code = code.replace(/--\[\[[\s\S]*?\]\]/g, '');
-        code = code.replace(/--[^\n]*/g, '');
-        return code;
-    }
-
     minify(code) {
         let result = code;
-        // Remove empty lines
-        result = result.split('\n').map(line => line.trim()).filter(line => line.length > 0).join('\n');
-        // Collapse newlines to space
-        result = result.replace(/\n/g, ' ');
-        // Collapse multiple spaces
+        result = result.split('\n').map(line => line.trim()).filter(line => line.length > 0).join(' ');
         result = result.replace(/\s+/g, ' ');
-        // Remove spaces around certain operators
         result = result.replace(/\s*([{}()\[\],;])\s*/g, '$1');
-        // Ensure spaces around keywords
         result = result.replace(/\b(and|or|not|then|do|end|else|elseif|in|local|function|return|if|while|for|repeat|until)\b/g, ' $1 ');
-        // Clean up multiple spaces
         result = result.replace(/\s+/g, ' ');
         return result.trim();
     }
